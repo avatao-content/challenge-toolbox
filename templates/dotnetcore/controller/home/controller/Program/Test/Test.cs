66dbaa31e837470b7f5e71a6fa6bd52dcf96fd87
Geri <gergo.turcsanyi@avatao.com>
2017-11-16 16:43:56 +0100

    .NET Core template ADDED

---
+++ templates/dotnetcore/controller/home/controller/Program/Test/Test.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using App;
+
+namespace UnitTests
+{
+    public class Test
+    {
+        [Fact]
+        public void CorrectHello()
+        {
+            Assert.Equal("Hello, World!!", Program.hello());
+        }
+
+        [Fact]
+        public void AnotherCorrectHello()
+        {
+            Assert.Equal("Hello, World!!", Program.hello());
+        }
+    }
+}
---
+++ templates/dotnetcore/solvable/home/user/solvable/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace App
+{
+    public class Program
+    {
+        public static string hello() {
+            return "Hello World!";
+        }
+    }
+}
