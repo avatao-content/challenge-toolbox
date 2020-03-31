63e7d0fa2bea153d604aa976d35709a588f985af
David Osztertag <root@oszi.one>
2020-03-31 14:43:20 +0200

    Increase min docker memory limit to 8 MiB

--- toolbox/docker/check.py
+++ toolbox/docker/check.py
@@ -34,8 +34,8 @@ def check_config(config: dict):  # pylint: disable=too-many-branches
                               invalid_caps, CAPABILITIES)
 
         if 'mem_limit_mb' in item:
-            if not str(item['mem_limit_mb']).isnumeric() or not 4 < int(item['mem_limit_mb']) < 1024:
-                counted_error('Invalid mem_limit_mb: %s. It should be an integer between 4 and 1024 MegaBytes.',
+            if not str(item['mem_limit_mb']).isnumeric() or not 8 < int(item['mem_limit_mb']) < 1024:
+                counted_error('Invalid mem_limit_mb: %s. It should be an integer between 8 and 1024 MegaBytes.',
                               item['mem_limit_mb'])
 
         if 'volumes' in item:
